c01698216f525bfccca9952d2bd68d3f71bf94a7
Ammyy9908 <sb78639@gmail.com>
2021-10-18 00:04:14 +0530

    rollup fixed

--- rollup.config.js
+++ rollup.config.js
@@ -4,6 +4,7 @@ import commonjs from "@rollup/plugin-commonjs";
 import livereload from "rollup-plugin-livereload";
 import { terser } from "rollup-plugin-terser";
 import css from "rollup-plugin-css-only";
+import ghPages from "gh-pages";
 
 const production = !process.env.ROLLUP_WATCH;
 
